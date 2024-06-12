c356afbdd1ce93f08a37788c4c82045a48fe0030
kdohyeon <kdohyeon@gmail.com>
2024-06-12 21:20:01 +0900

    commit

--- src/main/java/fast/campus/fcss04/config/SecurityConfig.java
+++ src/main/java/fast/campus/fcss04/config/SecurityConfig.java
@@ -2,12 +2,19 @@ package fast.campus.fcss04.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class SecurityConfig {
@@ -16,8 +23,19 @@ public class SecurityConfig {
         return new JdbcUserDetailsManager(dataSource);
     }
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder(2, 2, 2, 2, 2));
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
